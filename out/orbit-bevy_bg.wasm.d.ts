a939399f0cb85c2fa4ca3b3a58250f043b29bb2f
farpo <farpoproject@gmail.com>
2025-04-24 19:35:37 +0200

    maybe lol

---
+++ out/orbit-bevy_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he89e6fdf00325aca: (a: number, b: number, c: number) => void;
+export const closure2654_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__haee8d5adcd5d086b: (a: number, b: number) => void;
+export const closure2658_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd32eedcdebd9baa5: (a: number, b: number) => void;
+export const closure99497_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
